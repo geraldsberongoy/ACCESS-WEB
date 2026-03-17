a6028b8f060c01cffce9aa73a773ea2490735445
Gerald Berongoy <geraldberongoy04@gmail.com>
2026-03-17 19:56:53 +0800

    feat: initialize Open Next for Cloudflare and update build script in package.json

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 
 const nextConfig: NextConfig = {
+  outputFileTracingRoot: process.cwd(),
   images: {
     unoptimized: true,
   },
 };
 
 export default nextConfig;
+
+initOpenNextCloudflareForDev();
