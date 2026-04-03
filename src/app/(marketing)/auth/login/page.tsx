55dc1bd27d146c48ce66f321788395a857fa2f94
RenzArcilla <renzarcilla09@gmail.com>
2026-04-03 16:36:05 +0800

    feat: add auth route pages

---
+++ src/app/(marketing)/auth/login/page.tsx
@@ -0,0 +1,33 @@
+import { signIn } from "@/features/auth/actions/auth.actions";
+import { AuthForms } from "@/features/auth/components/AuthForms";
+import Link from "next/link";
+
+const LoginPage = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-black">
+      <div className="w-full max-w-md space-y-8">
+        <div className="text-center">
+          <h1 className="text-3xl">Sign In </h1>
+          <p className="mt-2 text-sm">Welcome to ACCESS-WEB, please sign-in to your account</p>
+        </div>
+        <div className="bg-white-900 border border-gray-800 p-8 rounded-lg">
+          <AuthForms action={signIn}/>
+
+          <div className="text-sm text-center mt-6">
+            <span className="text-white-700">{"Dont have an account?"} </span>
+            <Link href={"/auth/register"} className="font-medium text-white underline">
+            Sign Up
+            </Link>
+            <div className="text-sm text-center mt-1">
+              <Link href={"/"} className="font-medium text-white underline">
+              Sign In as Guest
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LoginPage;
---
+++ src/app/(marketing)/auth/register/page.tsx
@@ -0,0 +1,32 @@
+import { signUp } from "@/features/auth/actions/auth.actions";
+import { AuthForms } from "@/features/auth/components/AuthForms";
+import Link from "next/link";
+
+const RegisterPage = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-black">
+      <div className="w-full max-w-md space-y-8">
+        <div className="text-center">
+          <h1 className="text-3xl">Register </h1>
+          <p className="mt-2 text-sm">Welcome to ACCESS-WEB, please register an account</p>
+        </div>
+        <div className="bg-white-900 border border-gray-800 p-8 rounded-lg">
+          <AuthForms action={signUp} isRegister/>
+          <div className="text-sm text-center mt-6">
+            <span className="text-white-700">Already have an account? </span>
+            <Link href={"/auth/login"} className="font-medium text-white underline">
+            Sign Up
+            </Link>
+            <div className="text-sm text-center mt-1">
+              <Link href={"/"} className="font-medium text-white underline">
+              Register as Guest
+              </Link>
+            </div>
+          </div> 
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default RegisterPage;
